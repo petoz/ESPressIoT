f5da001aa178f1a10b6c5f947c9973676e86dc36
petoz <peterzapletal@gmail.com>
2018-04-03 14:45:15 +0200

    removed double define relay pin lines

--- espressiot/heater.ino
+++ espressiot/heater.ino
@@ -5,7 +5,7 @@
 // Simplified Heater PWM - just connect SSR to HEAT_RELAY_PIN
 //
 
-#define HEAT_RELAY_PIN D5 // D5 + GND are close on NodeMCU
+//#define HEAT_RELAY_PIN D5 // D5 + GND are close on NodeMCU
 #define HEAT_RELAY_PIN D1 // D1 on wemos D1
 
 float heatcycles; // the number of millis out of 1000 for the current heat amount (percent * 10)
